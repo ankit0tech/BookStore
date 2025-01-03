33cf442723f0e10a28bbc7abecbc7ea35f84553b
Ankit Punia <ankitsingh.as495@gmail.com>
2025-01-03 05:46:02 +0530

    removed book_title from cart table

---
+++ backend/prisma/migrations/20250103001329_removed_book_title_from_cart/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `book_title` on the `cart` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "cart" DROP COLUMN "book_title";

bfa945df4f660a7fdb4b8b17fc717bfcdbe7cd0e
Ankit Punia <ankitsingh.as495@gmail.com>
2025-04-29 07:05:51 +0530

    updated addresses UI

--- frontend/src/pages/Addresses.tsx
+++ frontend/src/pages/Addresses.tsx
@@ -73,24 +73,39 @@ const Addresses = () => {
                 <div>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 transition-all duration-500 ">
                         {addresses && addresses.map((address, index) => (
-                            <div key={index} className="m-4 p-4 border rounded-lg">
-                                <div>{address.house_number}</div>
-                                <div>{address.street_address}</div>
-                                <div className="w-full">{address.city}, {address.state} {address.zip_code}</div>
-                                {!address.is_default && 
+                            <div key={index} className="m-4 p-6 flex flex-col border rounded-lg hover:shadow">
+                                <div className="space-y-2">
+                                    <div className="font-medium">{address.house_number}</div>
+                                    <div className="text-gray-600">{address.street_address}</div>
+                                    <div className="text-gray-600 w-full">{address.city}, {address.state} {address.zip_code}</div>
+                                    <div className="text-gray-600">{address.country}</div>
+                                </div>
+
+                                {!address.is_default ? (
                                     <button 
-                                        className="mt-2 border rounded-md px-2 hover:bg-gray-200 text-gray-800"
+                                        className="mt-4 px-4 py-2 w-fit rounded-md text-blue-600 bg-blue-50 hover:bg-blue-100 transition-colors"
                                         onClick={() => handleMakeAddressDefault(address.id)}
                                     >
                                         Make default
-                                    </button>}
-                                {address.is_default && <div className="mt-2 text-gray-800">Default Address</div>}
-                                <div className="flex py-4">
-                                    <Link to={`/dashboard/address/update/${address.id}`}>
-                                        <AiOutlineEdit className="m-1 text-xl text-yellow-600" />
+                                    </button> 
+                                ): (
+                                    <div className="mt-4 px-4 py-2 w-fit rounded-md inline-block text-green-600 bg-green-50 hover:bg-green-100">
+                                        Default Address
+                                    </div>
+                                )}
+                                
+                                <div className="flex mt-4 gap-4">
+                                    <Link 
+                                        className="tex-xl text-yellow-600 hover:text-yellow-700" 
+                                        to={`/dashboard/address/update/${address.id}`}
+                                    >
+                                        <AiOutlineEdit className="text-xl" />
                                     </Link>
-                                    <Link to={`/dashboard/address/delete/${address.id}`}>
-                                        <MdOutlineDelete className="m-1 text-xl text-red-600" />
+                                    <Link 
+                                        className="tex-xl text-red-600 hover:text-red-700" 
+                                        to={`/dashboard/address/delete/${address.id}`}
+                                    >
+                                        <MdOutlineDelete className="text-xl" />
                                     </Link>
                                 </div>
                             </div>

5a407b014c3cef65d906be591375ef840c744c43
Ankit Punia <ankitsingh.as495@gmail.com>
2025-05-04 12:10:44 +0530

    updated cart page

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -7,6 +7,8 @@ import { CartInterface } from "../types";
 import BackButton from '../components/BackButton'
 import Spinner from "../components/Spinner";
 import { BiMinus, BiPlus } from "react-icons/bi";
+import { AiOutlineDelete } from "react-icons/ai";
+import { MdOutlineDelete } from "react-icons/md";
 // import { setCartItems as setCartItemsSlice } from "../redux/cartSlice";
 // import { enqueueSnackbar } from "notistack";
 
@@ -23,46 +25,64 @@ const Cart = () => {
 
     return(
         <div className="p-4">
-           <BackButton />
+           {/* <BackButton /> */}
             {
                 !cartItems.data.length ? (
                     <div className="p-4">Cart is Empty</div>
                 ) : (
-                    <div>
-                        <h2 className="text-xl font-semibold my-4">Cart Items</h2>
+                    <div className="overflow-x-auto">
+                        <h2 className="text-2xl font-semibold mb-6">Cart Items</h2>
                         <ul>
                             {cartItems.data.map((item) => (
-                                <li className="flex justify-between max-w-full sm:max-w-[80vw] border rounded-lg m-4 p-4 border-2 rounded-lg px-4 relative hover:shadow-xl" key={item.book.id}>
+                                <li className="overflow-x-auto flex justify-between max-w-full sm:max-w-[80vw] border-b my-4 py-4" key={item.book.id}>
                                     <div className="flex justify-between">
-                                        <div className="w-36 h-48 bg-gray-100 rounded-lg shadow-md overflow-hidden flex justify-center items-center">
+                                        <div className="w-36 h-48 rounded-lg overflow-hidden flex justify-center items-center">
                                             <img
                                                 src={item.book.cover_image}
                                                 alt="book cover"
                                                 className="w-full h-full object-cover object-scale-down" 
                                             ></img>
                                         </div>
-                                        <div className="p-2">
-                                            { item.book.title }
-                                        </div>
-                                    </div>
-                                    <div className="">
-                                        <div className="border rounded-lg">
-                                            <div className="p-2">
-                                                Qty: { item.quantity }
-                                            </div>
-                                            <div className="p-2">
+                                        <div className="flex flex-col p-4 gap-2">
+                                            <p>{ item.book.title }</p>
+                                            <p>{ item.book.author }</p>
+                                            <p className="">
                                                 Total: { item.book.price * item.quantity }
+                                            </p>
+
+                                            <div className="inline-flex items-center rounded-full border-2 border-purple-500 w-fit">
+                                                <button 
+                                                    className="p-1 px-4"
+                                                    onClick={() => {handleCartUpdate(item.book.id, -1)}}    
+                                                >
+                                                    {item.quantity === 1 ? <MdOutlineDelete className="text-xl" /> : <BiMinus className="text-xl" /> }
+                                                </button>
+
+                                                <p className="p-1">
+                                                    { item.quantity }
+                                                </p>
+
+                                                <button 
+                                                    className="p-1 px-4" 
+                                                    onClick={() => {handleCartUpdate(item.book.id, 1)}}
+                                                >
+                                                    <BiPlus className="text-xl"  />
+                                                </button>
                                             </div>
-                                            <div className="p-2 flex justify-around">
-                                                <BiMinus onClick={() => {handleCartUpdate(item.book.id, -1)}} />
-                                                <BiPlus onClick={() => {handleCartUpdate(item.book.id, 1)}} />
-                                            </div>
+                                            
                                         </div>
                                     </div>
                                 </li>
                             ))}
                         </ul>
-                        <button type='button' onClick={() => navigate('/dashboard/checkout')}  className="mx-2 mt-4 bg-purple-500 text-white px-3 py-2 rounded-full font-bold hover:bg-purple-700">Checkout</button>
+
+                        <button 
+                            type='button' 
+                            onClick={() => navigate('/dashboard/checkout')}  
+                            className="m-4 bg-blue-500 text-white px-3 py-2 rounded-md font-bold hover:bg-blue-600 transition-all duration-200"
+                        >
+                            Checkout
+                        </button>
 
                     </div>
                 )

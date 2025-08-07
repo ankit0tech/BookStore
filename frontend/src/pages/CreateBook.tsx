8ed3ed388bbd6b98279c35e1d259cc788caf3fa6
Ankit Punia <ankitsingh.as495@gmail.com>
2025-08-07 08:26:04 +0530

    updated create book page and api call

--- frontend/src/pages/CreateBook.tsx
+++ frontend/src/pages/CreateBook.tsx
@@ -7,14 +7,25 @@ import { Category } from '../types';
 
 const CreateBook = () => {
     const {id} = useParams();
-    const [title, setTitle] = useState('');
-    const [author, setAuthor] = useState('');
-    const [publishYear, setPublishYear] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState<string>('');
+    const [author, setAuthor] = useState<string>('');
+    const [publishYear, setPublishYear] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const [price, setPrice] = useState<string>('');
     const [categories, setCategories] = useState<Category[]|null>([]);
     const [selectedCategory, setSelectedCategory] = useState<string|null>(null);
-    const [imgUrl, setImgUrl] = useState('');
+    const [imgUrl, setImgUrl] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [isbn, setIsbn] = useState<string>('');
+    const [publisher, setPublisher] = useState<string>('');
+    const [language, setLanguage] = useState<string>('');
+    const [pages, setPages] = useState<string>('');
+    const [format, setFormat] = useState<string>('');
+    const [quantity, setQuantity] = useState<string>('');
+    const [isActive, setIsActive] = useState<boolean>(true);
+    const [shelfLocation, setShelfLocation] = useState<string>('');
+    const [sku, setSku] = useState<string>('');
+
     const [updateBook, setUpdateBook] = useState<boolean>(false);
     const [formErrors, setFormErrors] = useState<Record<string, string>>({});
     const navigate = useNavigate();
@@ -52,17 +63,28 @@ const CreateBook = () => {
             newErrors.author = 'Author is required';
         }
 
-        if(!publishYear.trim()) {
+        if(isNaN(Number(publishYear))) {
             newErrors.publishYear = 'Publish year is requred';
         }
 
-        if(!price.trim()) {
-            newErrors.price = 'Price is required';
+        if(isNaN(Number(price)) || Number(price) < 0) {
+            newErrors.price = 'Price is invalid';
         }
 
-        if(!selectedCategory?.trim()) {
-            newErrors.selectedCategory = 'Category is required';
+        if(isNaN(Number(selectedCategory)) || Number(selectedCategory) <= 0) {
+            newErrors.selectedCategory = 'Category is invalid';
         }
+
+        if(pages != '' && (isNaN(Number(pages)) || Number(pages) <= 0)) {
+            newErrors.pages = 'Invalid number of pages'
+        }
+        
+        if(quantity && (isNaN(Number(quantity)) || Number(quantity) <= 0)) {
+            newErrors.quantity = 'Quantity is invlaid';
+        }
+
+
+
         
 
         if(!imgUrl.trim()) {
@@ -92,7 +114,17 @@ const CreateBook = () => {
             publish_year: Number(publishYear),
             price: Number(price),
             category_id: Number(selectedCategory),
-            cover_image: imgUrl
+            cover_image: imgUrl,
+            description: description,
+            isbn: isbn,
+            publisher: publisher,
+            language: language,
+            pages: Number(pages),
+            format: format,
+            quantity: Number(quantity),
+            is_active: isActive,
+            shelf_location: shelfLocation,
+            sku: sku
         };
         
         console.log(data);
@@ -150,6 +182,17 @@ const CreateBook = () => {
             setPrice(response.data.price.toString());
             setSelectedCategory(response.data.category_id?.toString() || null);
             setImgUrl(response.data.cover_image);
+            setDescription(response.data.description);
+            setIsbn(response.data.isbn);
+            setPublisher(response.data.publisher);
+            setLanguage(response.data.language);
+            setPages(response.data.pages);
+            setFormat(response.data.format);
+            setQuantity(response.data.quantity);
+            setIsActive(response.data.is_active);
+            setShelfLocation(response.data.shelf_location);
+            setSku(response.data.sku);
+
             setLoading(false);
         })
         .catch((error)=>{
@@ -259,6 +302,126 @@ const CreateBook = () => {
                         </input>
                         { formErrors.imgUrl && (<p className='text-sm text-red-500 mt-1'> {formErrors.imgUrl} </p>)}
                     </div>
+
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Description</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.description? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type="text"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.description && (<p className='text-sm text-red-500 mt-1'> {formErrors.description} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>ISBN</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.isbn? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type="text"
+                            value={isbn}
+                            onChange={(e) => setIsbn(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.isbn && (<p className='text-sm text-red-500 mt-1'> {formErrors.isbn} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Publisher</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.publisher? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type="text"
+                            value={publisher}
+                            onChange={(e) => setPublisher(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.publisher && (<p className='text-sm text-red-500 mt-1'> {formErrors.publisher} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Language</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.language? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type="text"
+                            value={language}
+                            onChange={(e) => setLanguage(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.language && (<p className='text-sm text-red-500 mt-1'> {formErrors.language} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Pages</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.pages? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type="number"
+                            min={0}
+                            value={pages}
+                            onChange={(e) => setPages(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.pages && (<p className='text-sm text-red-500 mt-1'> {formErrors.pages} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Format</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.format? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type="text"
+                            value={format}
+                            onChange={(e) => setFormat(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.format && (<p className='text-sm text-red-500 mt-1'> {formErrors.format} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Quantity</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.quantity? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type="number"
+                            min={0}
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.quantity && (<p className='text-sm text-red-500 mt-1'> {formErrors.quantity} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Book Status</label>
+                        <div className='flex items-center mt-2 space-x-3'>
+                            <button 
+                                className={`inline-flex items-center rounded-full transition-colors w-11 h-6 ${isActive ? 'bg-blue-600' : 'bg-gray-200'} focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}
+                                type='button'  
+                                onClick={() => setIsActive(!isActive)}  
+                            >
+                                <span className={`inlne-block rounded-full h-4 w-4 bg-white transform transition-transform ${isActive ? 'translate-x-6' : 'translate-x-1'}`}></span>
+                            </button>
+                            <span className={`text-md ${isActive ? 'text-green-500' : 'text-gray-500'}`}>{isActive ? 'Active' : 'Inactive'}</span>
+                        </div>
+                        {formErrors.isActive && (
+                            <p className='text-sm text-red-500 mt-1'>{formErrors.isActive}</p>
+                        )}
+                    </div>
+
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Shelf Location</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.shelfLocation? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type='text'
+                            value={shelfLocation}
+                            onChange={(e) => setShelfLocation(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.shelfLocation && (<p className='text-sm text-red-500 mt-1'> {formErrors.shelfLocation} </p>)}
+                    </div>
+                    <div className=''>
+                        <label className='text-sm font-semibold text-gray-600'>Stock Keeping Unit (SKU)</label>
+                        <input
+                            className={`w-full rounded-lg mt-2 px-4 py-2 border ${formErrors.sku? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:border-blue-400`}
+                            type='text'
+                            value={sku}
+                            onChange={(e) => setSku(e.target.value)}
+                        >
+                        </input>
+                        { formErrors.sku && (<p className='text-sm text-red-500 mt-1'> {formErrors.sku} </p>)}
+                    </div>
+
                 </div>
 
                 <div className="flex flex-row justify-end gap-2">

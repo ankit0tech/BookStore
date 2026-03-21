6a02cd78e5175959ed3b1d67089c8361b4a51486
Ankit Punia <ankitsingh.as495@gmail.com>
2026-03-21 11:01:34 +0530

    improved user details and update user pages

--- frontend/src/components/DropDownMenu.tsx
+++ frontend/src/components/DropDownMenu.tsx
@@ -17,7 +17,7 @@ const DropDownMenu = <T extends string> ({title, defaultValue, selectedOptionSta
 
     return (
         <div 
-            className="w-full relative border rounded-md text-gray-800"
+            className="w-full relative border border-gray-300 hover:border-gray-400 rounded-sm text-gray-800"
         >
             <button
                 className="w-full text-nowrap px-3 py-2 flex flex-row gap-2 items-center justify-between"
